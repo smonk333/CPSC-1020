d9f2d0b8ebddcb50f138f6acee78b00d58ea003b
garrett <gboling829@gmail.com>
2022-09-12 17:14:06 -0400

    wrote some code

---
+++ lab2/hospitalChargesCalculator/hospitalCharges.cpp
@@ -0,0 +1,23 @@
+// program to determine the cost of a hospital stay given information from the user
+// Garrett Boling
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+
+// declaring variables
+  int patient;
+  int daysIn;
+  float roomRate;
+  float labFees;
+  float medicationCharge;
+  float total;
+
+//prompting the user for data
+  cout << "This program will calculate a patient\'s hospital charges." << endl;
+
+}
