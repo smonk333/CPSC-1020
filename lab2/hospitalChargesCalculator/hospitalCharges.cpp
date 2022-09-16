ebac808f774013947aed931d455a52ff27658f46
Garrett Boling <gboling829@gmail.com>
2022-09-16 12:32:22 -0400

    fixed output formatting

--- lab2/hospitalChargesCalculator/hospitalCharges.cpp
+++ lab2/hospitalChargesCalculator/hospitalCharges.cpp
@@ -78,7 +78,7 @@ int main(){
 
 //calculate the total charges and display the total before ending the program
     total = calcTotalCharges(daysIn, roomRate, labFees, medicationCharge);
-    cout << "Your total hospital bills is $" << fixed <<setprecision(2) << total;
+    cout << "Your total hospital bills is $" << fixed <<setprecision(2) << total << endl;
   }
 
 //if the patient is an outpatient...
@@ -106,7 +106,7 @@ int main(){
 
 //calculate the total charges and display the total before ending the program
     total = calcTotalCharges(labFees, medicationCharge);
-    cout << "Your total hospital bills is $" << fixed <<setprecision(2) << total;
+    cout << "Your total hospital bills is $" << fixed <<setprecision(2) << total << endl;
   }
 
   return 0;
