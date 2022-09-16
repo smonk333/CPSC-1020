3c76e6c2ff4ac01abd6eca3ba4f6fc4c65845cbe
Garrett Boling <gboling829@gmail.com>
2022-09-16 11:39:50 -0400

    added code

--- lab2/hospitalChargesCalculator/hospitalCharges.cpp
+++ lab2/hospitalChargesCalculator/hospitalCharges.cpp
@@ -4,20 +4,44 @@
 #include <iostream>
 #include <cmath>
 #include <iomanip>
+#include "validateUserInput.h"
+#include "calcTotalCharges.h"
 
 using namespace std;
 
+// main goes here
 int main(){
 
 // declaring variables
-  int patient;
+  char patient;
   int daysIn;
   float roomRate;
   float labFees;
   float medicationCharge;
   float total;
 
-//prompting the user for data
+// prompting the user for patient type
   cout << "This program will calculate a patient\'s hospital charges." << endl;
+  cout << "Enter I for in-patient or O for out-patient: " << endl;
+
+// take in the data
+  cin >> patient;
+
+// input validation, because reading is hard sometimes
+  while (patient != 'I' && patient != 'O'){
+    cout << "ERROR Please enter I for in-patient or O for out-patient:" << endl;
+    cin >> patient;
+  }
+
+// check for patient type, perform appropriate calculations, and validate their inputs
+  if (patient == 'I'){
+    cout << "Number of days in the hospital: " << endl;
+    cin >> daysIn;
+
+    while (!validateUserInput(daysIn)) {
+            cout << "ERROR: Number cannot be negative" << endl;
+            cin >> daysIn;
+        }
+  }
 
 }
---
+++ lab2/hospitalChargesCalculator/validateUserInput.cpp
@@ -0,0 +1,19 @@
+#include "validateUserInput.h"
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+bool validateUserInput(float in){
+//declare a boolean for returning the results of the check
+    bool output {};
+
+//check to see if the input is greater than 0 (non-negative)
+    if (in > 0)
+         output = true;
+    else if (in < 0)
+        output = false;
+    
+//return the results of the check
+    return output;
+}
