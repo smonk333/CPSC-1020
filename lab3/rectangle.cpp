0e34566b41d86eb42bef96a2aab235575dcdc60a
Garrett Boling <gboling829@gmail.com>
2022-09-23 18:33:31 -0400

    rearranged function definitions for aesthetics

---
+++ lab3/rectangle.cpp
@@ -0,0 +1,25 @@
+#include "rectangle.h"
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double rectangle::calcArea(){
+    
+}
+
+double rectangle::getLength(){
+
+}
+
+double rectangle::getWidth(){
+
+}
+
+bool rectangle::setLength(double l){
+
+}
+
+bool rectangle::setWidth(double w){
+
+}
