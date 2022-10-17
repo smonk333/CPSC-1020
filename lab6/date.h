a70491090c8a190befafb8e78f9b41cf29fd9df8
garrett <gboling829@gmail.com>
2022-10-17 16:47:03 -0400

    began writing lab6

---
+++ lab6/date.h
@@ -0,0 +1,14 @@
+//
+// Created by gboling on 10/17/22.
+//
+
+#ifndef CPSC_1020_DATE_H
+#define CPSC_1020_DATE_H
+
+
+class date {
+
+};
+
+
+#endif //CPSC_1020_DATE_H
---
+++ lab6/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by gboling on 10/17/22.
+//
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include "date.h"
+
+int main(int argc, char const *argv[]){
+
+}
